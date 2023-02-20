88f02afadc09460fff5e1c56eb0b63fcbd9c4536
Alireza Safaierad <frontendmonster@gmail.com>
2023-02-20 12:26:59 +0400

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+const { init } = require('@fullstacksjs/eslint-config/init');
+
+module.exports = init({
+  modules: {
+    auto: true,
+  },
+  root: true,
+});
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  clearScreen: false,
+  server: {
+    port: 1420,
+    strictPort: true,
+  },
+  // to make use of `TAURI_DEBUG` and other env variables
+  // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
+  envPrefix: ["VITE_", "TAURI_"],
+  build: {
+    target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
+    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
+});
