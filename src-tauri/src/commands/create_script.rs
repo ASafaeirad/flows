a15341041faab7aefe284a2e527d897581c6b940
Alireza Safaierad <frontendmonster@gmail.com>
2023-03-08 17:51:27 +0400

    edit when file exists

--- src-tauri/src/commands/create_script.rs
+++ src-tauri/src/commands/create_script.rs
@@ -1,4 +1,20 @@
+use std::fs;
+
+use edit::{edit_file};
+
+use crate::config::Config;
+
 #[tauri::command]
-pub fn create_script(name: &str) -> String {
-  name.to_owned()
+pub fn create_script(name: &str) -> Result<(), &str> {
+    let config = Config::new();
+    let mut file = config.script_path.join(name.to_lowercase());
+    file.set_extension("ts");
+
+    if fs::read(&file).is_err() {
+        edit_file(file).map_err(|_e| "Cannot open the file")?;
+        return Ok(());
+    }
+
+    fs::File::create(file).map_err(|_e| "Cannot create the file")?;
+    return Ok(())
 }
